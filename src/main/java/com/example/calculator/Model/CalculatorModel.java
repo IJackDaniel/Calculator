9c44040119339888aacadb056051c9de0b0be6c5
Danya PC <danya05af@gmail.com>
2025-06-14 13:37:49 +0300

    written the addition and rounding functions

--- src/main/java/com/example/calculator/Model/CalculatorModel.java
+++ src/main/java/com/example/calculator/Model/CalculatorModel.java
@@ -22,10 +22,33 @@ public class CalculatorModel {
         return currentInput;
     }
 
-    // Checks
-    public boolean isNotZero(double num) {
+    // Checks and internal operations
+    private boolean isNotZero(double num) {
         return num != 0;
     }
 
+    private boolean isInteger(double num) {
+        return num == (int) num;
+    }
+
+    private double round(double value, int places) {
+        double scale = Math.pow(10, places);
+        return Math.round(value * scale) / scale;
+    }
+
+
     // Operations
+    public void roundAccumulator() {
+        if (isInteger(this.currentInput)) {
+            this.accumulator = round(this.accumulator, (int) this.currentInput);
+        }
+        else {
+            System.out.println("Second number is not Integer!");
+        }
+    }
+
+    public void add() {
+        this.accumulator = round(this.accumulator + this.currentInput, 4);
+        this.currentInput = 0;
+    }
 }
